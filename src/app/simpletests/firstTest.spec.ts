9bcc758c4912c20586ed3845920d16573a730e6e
keithspurr <keithspurr@ksqdesign.com>
2020-09-20 15:49:38 +0100

    unit test one

---
+++ src/app/simpletests/firstTest.spec.ts
@@ -0,0 +1,10 @@
+describe('First Test', () => {
+  it('should pass', () => {
+    expect(1 + 1).toEqual(2);
+  });
+});
+describe('Second Test group', () => {
+  it('should fail', () => {
+    expect(1 - 1).toEqual(1);
+  });
+});
